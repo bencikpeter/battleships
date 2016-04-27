539a6e27d9141ba6240fc52fd27f413968e723ce
bencikpeter <bencik.peter.ml@gmail.com>
2016-04-27 16:45:18 +0200

    Headers for backend created

---
+++ battleships/business_logic_backend.h
@@ -0,0 +1,16 @@
+//
+//  business_logic_backend.h
+//  battleships
+//
+//  Created by Peter Bencik on 27.04.16.
+//  Copyright © 2016 Peter Bencik. All rights reserved.
+//
+
+#ifndef business_logic_backend_h
+#define business_logic_backend_h
+
+namespace logic {
+    <#declarations#>
+}
+
+#endif /* business_logic_backend_h */
---
+++ battleships/network_backend.h
@@ -0,0 +1,16 @@
+//
+//  network_backend.h
+//  battleships
+//
+//  Created by Peter Bencik on 27.04.16.
+//  Copyright © 2016 Peter Bencik. All rights reserved.
+//
+
+#ifndef network_backend_h
+#define network_backend_h
+
+namespace network {
+    <#declarations#>
+}
+
+#endif /* network_backend_h */
