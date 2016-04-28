4993b93315df8abe37381e5371dc8779b3d8b7e1
Moroxus <lukas.tomovic@gmail.com>
2016-04-28 17:34:42 +0200

    added #include for SDL2 and SDL2_net

--- battleships/main.cpp
+++ battleships/main.cpp
@@ -6,8 +6,14 @@
 //  Copyright © 2016 Peter Bencik. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include "SDL.h"
+#include "SDL_net.h"
 
 int main(){
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDLNet_Init();
 
+    SDLNet_Quit();
+    SDL_Quit();
 }
