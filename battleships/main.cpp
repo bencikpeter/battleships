d3fbe33dcae1a632bb8da92be0ff6912ee016bc0
Moroxus <lukas.tomovic@gmail.com>
2016-04-29 09:34:24 +0200

    first version of main loop

--- battleships/main.cpp
+++ battleships/main.cpp
@@ -11,7 +11,46 @@
 #include "SDL_net.h"
 
 int main(){
+    using std::cout;
+    using std::endl;
+
     SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window* window;
+    window = SDL_CreateWindow( "Battleships !",
+                               SDL_WINDOWPOS_UNDEFINED,
+                               SDL_WINDOWPOS_UNDEFINED,
+                               400, // window width
+                               200, // window height
+                               SDL_WINDOW_RESIZABLE );
+
+    if ( window == nullptr ) {
+        cout << "There was error initializing the window !" << endl;
+    }
+
+    SDL_Surface* screen = SDL_GetWindowSurface( window );
+
+    Uint32 white = SDL_MapRGB( screen->format, 255, 255, 255 );
+
+    SDL_FillRect( screen, nullptr, white );
+
+    SDL_UpdateWindowSurface( window );
+
+    SDL_Event event;
+    bool running = true;
+
+    while ( running ) {
+        while( SDL_PollEvent( &event ) ) {
+            if ( event.type == SDL_QUIT ) {
+                running = false;
+                break;
+            }
+        }
+
+    }
+
+    SDL_DestroyWindow( window );
+
     SDLNet_Init();
 
     SDLNet_Quit();
