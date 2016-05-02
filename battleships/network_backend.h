5cd32de553bcedb12f2371d5467b682d0fd39fce
bencikpeter <bencik.peter.ml@gmail.com>
2016-05-02 18:02:56 +0200

    local address handling finished

--- battleships/network_backend.h
+++ battleships/network_backend.h
@@ -15,6 +15,10 @@
 #include <vector>
 #include <exception>
 #include <regex>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include "../asio-1.10.6/include/asio.hpp"
 
@@ -125,19 +129,21 @@ namespace network {
     private: //TEST THIS STUFF
         bool isInitMessage(std::vector<char> message){
             std::string toBeTested = toString(message);
-            std::regex reg ("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$");
+            //std::regex reg ("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+            std::regex reg("^(\d+)\.(\d+)\.(\d+)\.(\d+)$"); //wont be sending numerical stuff, so this is sufficient
             return std::regex_match(toBeTested,reg);
         }
         
         std::vector<char> toCharVector(std::string const &_string){
             std::vector<char> message;
-            for (char& c : _string) {
+            for (char const & c : _string) {
                 message.push_back(c);
             }
             return message;
         }
         
         std::string toString(std::vector<char> message){
+            
             std::string _string;
             for(char &c : message){
                 _string+=c;
@@ -145,40 +151,43 @@ namespace network {
             return _string;
         }
         
-        std::string
+        std::string getMyIPAddress(){
+            
+            struct ifaddrs * addresses = nullptr;
+            struct ifaddrs * addressTemp=NULL;
+            void * tmpAddrPtr=NULL;
+            
+            getifaddrs(&addresses);
+            
+            for(addressTemp = addresses; addressTemp != nullptr; addressTemp = addressTemp->ifa_next){
+                if (!addressTemp->ifa_addr) {
+                    continue;
+                }
+                if (addressTemp->ifa_addr->sa_family == AF_INET){ //if valid IPv4
+                    tmpAddrPtr=&((struct sockaddr_in *)addressTemp->ifa_addr)->sin_addr;
+                    char addressBuffer[INET_ADDRSTRLEN];
+                    inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
+                    std::string textAddress;
+                    textAddress.assign(addressBuffer);
+                    if (isLocalAddress(textAddress)) {
+                        if (addresses!=NULL) freeifaddrs(addresses);
+                        return textAddress;
+                    }
+                }
+            }
+            
+            if (addresses!=NULL) freeifaddrs(addresses);
+            return std::string();
+            
+        }
+        
+        bool isLocalAddress(std::string const& address){
+            std::regex reg("(^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)");
+            return std::regex_match(address, reg);
+        }
         
     };
     
-    std::string getMyIPAddress(){
-
-  struct ifaddrs * addresses = nullptr;
-  struct ifaddrs * addressTemp=NULL;
-  void * tmpAddrPtr=NULL;
-  
-  getifaddrs(&ifAddrStruct);
-
-  for(addressTemp = addresses; addressTemp != nullptr; addressTemp = addressTemp->ifa_next){
-    if (!ifa->ifa_addr) {
-      continue;
-    }
-    if (ifa->ifa_addr->sa_family == AF_INET){ //if valid IPv4
-      tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
-      char addressBuffer[INET_ADDRSTRLEN];
-      inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
-      std::string textAddress;
-      if(addressBuffer!=nullptr) textAddress(addressBuffer);
-      if (isLocalAddress(textAddress)) {
-        if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);  
-        return textAddress;
-      }
-    }
-  }
-  
-  if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);
-  
-}
-
-bool isLocalAddress(std::string const& address);
-}
+}//networtk
 
 #endif /* network_backend_h */
