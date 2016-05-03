91ed59dfe98291208a0eaa728687371488081ddf
jurajvandor <juraj.vandor@gmail.com>
2016-05-03 16:15:00 +0200

    moved from business_logic_backend

---
+++ battleships/business_logic_matrix.h
@@ -0,0 +1,42 @@
+#ifndef BUSSINESS_LOGIC_MATRIX
+#define BUSSINESS_LOGIC_MATRIX
+namespace logic{
+enum CellType { WATER_NOT_SHOT, WATER_SHOT, SHIP_NOT_SHOT, SHIP_SHOT };
+class Matrix{
+    CellType** matrix;
+public:
+    Matrix() = delete;
+    Matrix(CellType** a): matrix(new CellType*[10]) {
+        for (int i = 0; i < 10; i++){
+            matrix[i] = new CellType[10];
+            for (int j = 0; j < 10; j++){
+                matrix[i][j] = a[i][j];
+            }
+        }
+    }
+    Matrix(const Matrix& o): matrix(new CellType*[10]) {
+        for (int i = 0; i < 10; i++){
+            matrix[i] = new CellType[10];
+            for (int j = 0; j < 10; j++){
+                matrix[i][j] = o.matrix[i][j];
+            }
+        }
+    }
+
+    CellType** get(){return matrix;}
+
+    const Matrix& operator= (Matrix o){
+        std::swap(o.matrix, matrix);
+        return *this;
+    }
+
+    ~Matrix(){
+        for (int i = 0; i < 10; i++){
+            delete[] matrix[i];
+        }
+        delete[] matrix;
+    }
+};
+}
+#endif // BUSSINESS_LOGIC_MATRIX
+
