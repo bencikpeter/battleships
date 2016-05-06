24b0f385e37a6b080be069c983cc4827e1f901af
jurajvandor <juraj.vandor@gmail.com>
2016-05-06 16:31:11 +0200

    added operator<< for testing

--- battleships/business_logic_matrix.h
+++ battleships/business_logic_matrix.h
@@ -1,7 +1,34 @@
 #ifndef BUSSINESS_LOGIC_MATRIX
 #define BUSSINESS_LOGIC_MATRIX
+#include <ostream>
 namespace logic{
 enum CellType { SHIP_NOT_SHOT, SHIP_SHOT, WATER_NOT_SHOT, WATER_SHOT, CLICKABLE, NOT_CLICKABLE };
+
+inline std::ostream& operator<<(std::ostream & os, CellType & t)
+{
+  switch (t) {
+  case SHIP_NOT_SHOT:
+      os << 'S';
+      break;
+  case SHIP_SHOT:
+      os << '#';
+      break;
+  case WATER_NOT_SHOT:
+      os << '.';
+      break;
+  case WATER_SHOT:
+      os << 'x';
+      break;
+  case CLICKABLE:
+      os << 'C';
+      break;
+  case NOT_CLICKABLE:
+      os << 'N';
+      break;
+  }
+  return os;
+}
+
 class Matrix{
     CellType** matrix;
 public:
@@ -36,7 +63,19 @@ public:
         }
         delete[] matrix;
     }
+
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& m){
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 10; j++){
+                os << m.matrix[i][j] << " ";
+            }
+            os << std::endl;
+        }
+        return os;
+    }
 };
+
+
 }
 #endif // BUSSINESS_LOGIC_MATRIX
 
